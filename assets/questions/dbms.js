c128a1bc5b9f2140b2036e910cea01f007cf7260
Kashyap Jhaveri <jhaverikashyap1998@gmail.com>
2020-05-26 18:23:50 -0400

    Sample question template added

---
+++ assets/questions/dbms.js
@@ -0,0 +1,212 @@
+const data = '[{\n' +
+    '    "questionId": 1,\n' +
+    '    "question": "It is the property that describes various characteristics of an entity.",\n' +
+    '    "options": {\n' +
+    '        "A" : "Column",\n' +
+    '        "B" : "Attribute",\n' +
+    '        "C" : "ER Diagram",\n' +
+    '        "D" : "Relationship"\n' +
+    '    },\n' +
+    '    "answer": "B"\n' +
+    '},\n' +
+    '{\n' +
+    '    "questionId": 2,\n' +
+    '    "question": "It is an association between entities.",\n' +
+    '    "options": {\n' +
+    '        "A" : "One to One",\n' +
+    '        "B" : "Specialization",\n' +
+    '        "C" : "Relation",\n' +
+    '        "D" : "Generalization"\n' +
+    '    },\n' +
+    '    "answer": "C"\n' +
+    '},\n' +
+    '{\n' +
+    '    "questionId": 3,\n' +
+    '    "question": "______ stores metadata about the structure of the database.",\n' +
+    '    "options": {\n' +
+    '        "A" : "Query Analyzer",\n' +
+    '        "B" : "Data Catalog",\n' +
+    '        "C" : "Physical database",\n' +
+    '        "D" : "Data Dictionary"\n' +
+    '    },\n' +
+    '    "answer": "D"\n' +
+    '},\n' +
+    '{\n' +
+    '    "questionId": 4,\n' +
+    '    "question": "It is the minimal super key.",\n' +
+    '    "options": {\n' +
+    '        "A" : "Candidate Key",\n' +
+    '        "B" : "Unique Key",\n' +
+    '        "C" : "Primary Key",\n' +
+    '        "D" : "Alternate Key"\n' +
+    '    },\n' +
+    '    "answer": "A"\n' +
+    '},\n' +
+    '{\n' +
+    '    "questionId": 5,\n' +
+    '    "question": "Foreign Key can be Null.",\n' +
+    '    "options": {\n' +
+    '        "A" : "False",\n' +
+    '        "B" : "True"\n' +
+    '    },\n' +
+    '    "answer": "B"\n' +
+    '},\n' +
+    '{\n' +
+    '    "questionId": 6,\n' +
+    '    "question": "All the primary keys should be super keys.",\n' +
+    '    "options": {\n' +
+    '        "A" : "True",\n' +
+    '        "B" : "False"\n' +
+    '    },\n' +
+    '    "answer": "A"\n' +
+    '},\n' +
+    '{\n' +
+    '    "questionId": 7,\n' +
+    '    "question": "What is the function of a database?",\n' +
+    '    "options": {\n' +
+    '        "A" : "to check all spellings",\n' +
+    '        "B" : "to output data",\n' +
+    '        "C" : "to check all input data",\n' +
+    '        "D" : "to collect and organize input data"\n' +
+    '    },\n' +
+    '    "answer": "D"\n' +
+    '},\n' +
+    '{\n' +
+    '    "questionId": 8,\n' +
+    '    "question": "What is the language that is used by most DBMSs for helping their users to access data?",\n' +
+    '    "options": {\n' +
+    '        "A" : "SQL",\n' +
+    '        "B" : "4GL",\n' +
+    '        "C" : "High level language",\n' +
+    '        "D" : "Query language"\n' +
+    '    },\n' +
+    '    "answer": "D"\n' +
+    '},\n' +
+    '{\n' +
+    '    "questionId": 9,\n' +
+    '    "question": "Information can be transferred between the DBMS and ______.",\n' +
+    '    "options": {\n' +
+    '        "A" : "Graphics program",\n' +
+    '        "B" : "Word processor program",\n' +
+    '        "C" : "Spreadsheet program",\n' +
+    '        "D" : "All of the above"\n' +
+    '    },\n' +
+    '    "answer": "D"\n' +
+    '},\n' +
+    '{\n' +
+    '    "questionId": 10,\n' +
+    '    "question": "______ can be used as a primary key for a student file.",\n' +
+    '    "options": {\n' +
+    '        "A" : "Social Security Number",\n' +
+    '        "B" : "Major",\n' +
+    '        "C" : "Class",\n' +
+    '        "D" : "GPA"\n' +
+    '    },\n' +
+    '    "answer": "A"\n' +
+    '},\n' +
+    '{\n' +
+    '    "questionId": 11,\n' +
+    '    "question": "A record management system ______.",\n' +
+    '    "options": {\n' +
+    '        "A" : "can be used to extract information stored in a computer file",\n' +
+    '        "B" : "always uses a list as its model",\n' +
+    '        "C" : "can handle many files of information at a time",\n' +
+    '        "D" : "both a and b"\n' +
+    '    },\n' +
+    '    "answer": "A"\n' +
+    '},\n' +
+    '{\n' +
+    '    "questionId": 12,\n' +
+    '    "question": "It is a command that lets user to change one or more fields in a record.",\n' +
+    '    "options": {\n' +
+    '        "A" : "modify",\n' +
+    '        "B" : "lookup",\n' +
+    '        "C" : "insert",\n' +
+    '        "D" : "update"\n' +
+    '    },\n' +
+    '    "answer": "A"\n' +
+    '},\n' +
+    '{\n' +
+    '    "questionId": 13,\n' +
+    '    "question": "Database management systems are intended to ______.",\n' +
+    '    "options": {\n' +
+    '        "A" : "establish relationship among records in different files",\n' +
+    '        "B" : "maintain data integrity",\n' +
+    '        "C" : "eliminate data redundancy",\n' +
+    '        "D" : "manage file access",\n' +
+    '        "E" : "all of the above"\n' +
+    '    },\n' +
+    '    "answer": "E"\n' +
+    '},\n' +
+    '{\n' +
+    '    "questionId": 14,\n' +
+    '    "question": "The language used application programs to request data from the DBMS is referred to as the ______",\n' +
+    '    "options": {\n' +
+    '        "A" : "DML",\n' +
+    '        "B" : "DDL",\n' +
+    '        "C" : "query language",\n' +
+    '        "D" : "none of the above"\n' +
+    '    },\n' +
+    '    "answer": "A"\n' +
+    '},\n' +
+    '{\n' +
+    '    "questionId": 15,\n' +
+    '    "question": "It is a collection of operations that performs single logical function in a database application.",\n' +
+    '    "options": {\n' +
+    '        "A" : "Concurrent operation",\n' +
+    '        "B" : "Durability",\n' +
+    '        "C" : "Transaction",\n' +
+    '        "D" : "Atomicity"\n' +
+    '    },\n' +
+    '    "answer": "C"\n' +
+    '},\n' +
+    '{\n' +
+    '    "questionId": 16,\n' +
+    '    "question": "The problem that is compounded when constraints involve several data items from different files are called ______.",\n' +
+    '    "options": {\n' +
+    '        "A" : "Security Problem",\n' +
+    '        "B" : "Durability Problem",\n' +
+    '        "C" : "Transaction Control Management Problem",\n' +
+    '        "D" : "Integrity Problem"\n' +
+    '    },\n' +
+    '    "answer": "D"\n' +
+    '},\n' +
+    '{\n' +
+    '    "questionId": 17,\n' +
+    '    "question": "Ensuring atomicity is the responsibility of the ______ component",\n' +
+    '    "options": {\n' +
+    '        "A" : "Buffer Manager",\n' +
+    '        "B" : "Transaction Manager",\n' +
+    '        "C" : "File Manager",\n' +
+    '        "D" : "DBA"\n' +
+    '    },\n' +
+    '    "answer": "B"\n' +
+    '},\n' +
+    '{\n' +
+    '    "questionId": 18,\n' +
+    '    "question": "Using relational model we design conceptual database design.",\n' +
+    '    "options": {\n' +
+    '        "A" : "True",\n' +
+    '        "B" : "False"\n' +
+    '    },\n' +
+    '    "answer": "B"\n' +
+    '},\n' +
+    '{\n' +
+    '    "questionId": 19,\n' +
+    '    "question": "Tables are required to have at least one column.",\n' +
+    '    "options": {\n' +
+    '        "A" : "True",\n' +
+    '        "B" : "False"\n' +
+    '    },\n' +
+    '    "answer": "A"\n' +
+    '},\n' +
+    '{\n' +
+    '    "questionId": 20,\n' +
+    '    "question": "In Relational Database data is stored as record types and the relationship is represented by set types.",\n' +
+    '    "options": {\n' +
+    '        "A" : "True",\n' +
+    '        "B" : "False"\n' +
+    '    },\n' +
+    '    "answer": "A"\n' +
+    '}\n' +
+    ']\n';
