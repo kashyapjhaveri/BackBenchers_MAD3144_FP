f3297047633b86406c032272c23a7990f84d2bed
kamal777682 <60160730+kamal777682@users.noreply.github.com>
2020-05-26 18:43:32 -0400

    [Kamal] iOS.js added

---
+++ assets/questions/iOS.js
@@ -0,0 +1,215 @@
+const data = '[{\n' +
+    '  "questionId": 1,\n' +
+    '  "question": "When an activity doesn\'t exist in memory it is in which state?",\n' +
+    '  "options": {\n' +
+    '    "A" : "Running State",\n' +
+    '    "B" : "Non-existent State",\n' +
+    '    "C" : "Starting State",\n' +
+    '    "D" : "Loading State"\n' +
+    '  },\n' +
+    '  "answer": "C"\n' +
+    '},\n' +
+    '  {\n' +
+    '    "questionId": 2,\n' +
+    '    "question": "Which of the following is NOT a state in the lifecycle of a service?",\n' +
+    '    "options": {\n' +
+    '      "A" : "Running",\n' +
+    '      "B" : "Paused",\n' +
+    '      "C" : "Starting",\n' +
+    '      "D" : "Destroyed"\n' +
+    '    },\n' +
+    '    "answer": "B"\n' +
+    '  },\n' +
+    '  {\n' +
+    '    "questionId": 3,\n' +
+    '    "question": "What is the function of FaceTime in iPhone?",\n' +
+    '    "options": {\n' +
+    '      "A" : "Video Calls",\n' +
+    '      "B" : "Editing Photos",\n' +
+    '      "C" : "Digital Photos",\n' +
+    '      "D" : "Taking Videos"\n' +
+    '    },\n' +
+    '    "answer": "A"\n' +
+    '  },\n' +
+    '  {\n' +
+    '    "questionId": 4,\n' +
+    '    "question": "iPhone is a line of Internet and Multimedia-enabled smartphones designed and marketed by ______. ",\n' +
+    '    "options": {\n' +
+    '      "A" : "Intel",\n' +
+    '      "B" : "Apple",\n' +
+    '      "C" : "Dell",\n' +
+    '      "D" : "IBM"\n' +
+    '    },\n' +
+    '    "answer": "B"\n' +
+    '  },\n' +
+    '  {\n' +
+    '    "questionId": 5,\n' +
+    '    "question": "Does the iPhone browser support flash applications?",\n' +
+    '    "options": {\n' +
+    '      "A" : "False",\n' +
+    '      "B" : "True"\n' +
+    '    },\n' +
+    '    "answer": "B"\n' +
+    '  },\n' +
+    '  {\n' +
+    '    "questionId": 6,\n' +
+    '    "question": "The iPhone camera can",\n' +
+    '    "options": {\n' +
+    '      "A" : "Autofocus",\n' +
+    '      "B" : "Make digital photos",\n' +
+    '      "C" : "Record videos",\n' +
+    '      "D" : "both B & C"\n' +
+    '    },\n' +
+    '    "answer": "D"\n' +
+    '  },\n' +
+    '  {\n' +
+    '    "questionId": 7,\n' +
+    '    "question": "The iPhone has a feature that activates when you rotate the device from portrait to landscape.",\n' +
+    '    "options": {\n' +
+    '      "A" : "Accelerometer",\n' +
+    '      "B" : "Special Sensor",\n' +
+    '      "C" : "Rotator",\n' +
+    '      "D" : "Shadow detector"\n' +
+    '    },\n' +
+    '    "answer": "A"\n' +
+    '  },\n' +
+    '  {\n' +
+    '    "questionId": 8,\n' +
+    '    "question": "iOS stands for?",\n' +
+    '    "options": {\n' +
+    '      "A" : "iPhone Operating System",\n' +
+    '      "B" : "Inter-network Operating System",\n' +
+    '      "C" : "Internet Operating System",\n' +
+    '      "D" : "None of the above"\n' +
+    '    },\n' +
+    '    "answer": "A"\n' +
+    '  },\n' +
+    '  {\n' +
+    '    "questionId": 9,\n' +
+    '    "question": "The IDE used in Swift is ______.",\n' +
+    '    "options": {\n' +
+    '      "A" : "Gas",\n' +
+    '      "B" : "Ld",\n' +
+    '      "C" : "Xcode",\n' +
+    '      "D" : "None of the above"\n' +
+    '    },\n' +
+    '    "answer": "C"\n' +
+    '  },\n' +
+    '  {\n' +
+    '    "questionId": 10,\n' +
+    '    "question": "To create constants in swift we use ______ keyword.",\n' +
+    '    "options": {\n' +
+    '      "A" : "let",\n' +
+    '      "B" : "int",\n' +
+    '      "C" : "constant",\n' +
+    '      "D" : "var"\n' +
+    '    },\n' +
+    '    "answer": "A"\n' +
+    '  },\n' +
+    '  {\n' +
+    '    "questionId": 11,\n' +
+    '    "question": "Which of the following framework is not used in iOS?",\n' +
+    '    "options": {\n' +
+    '      "A" : "AppKit Framework",\n' +
+    '      "B" : "CoreMotion Framework",\n' +
+    '      "C" : "UIKit Framework",\n' +
+    '      "D" : "Foundation Framework"\n' +
+    '    },\n' +
+    '    "answer": "A"\n' +
+    '  },\n' +
+    '  {\n' +
+    '    "questionId": 12,\n' +
+    '    "question": "Which of the following iOS frameworks is a commonly used third party library?",\n' +
+    '    "options": {\n' +
+    '      "A" : "Audiotoolbox.framework",\n' +
+    '      "B" : "CFNetwork.framework",\n' +
+    '      "C" : "AVFoundation.framework",\n' +
+    '      "D" : "AFNetwork.framework"\n' +
+    '    },\n' +
+    '    "answer": "D"\n' +
+    '  },\n' +
+    '  {\n' +
+    '    "questionId": 13,\n' +
+    '    "question": "Flash Applications is supported in iPhone browsers.",\n' +
+    '    "options": {\n' +
+    '      "A" : "Yes",\n' +
+    '      "B" : "No",\n' +
+    '      "C" : "It supports flash applications from apple only",\n' +
+    '      "D" : "Supports partially"\n' +
+    '    },\n' +
+    '    "answer": "A"\n' +
+    '  },\n' +
+    '  {\n' +
+    '    "questionId": 14,\n' +
+    '    "question": "Which of the following statement is wrong?",\n' +
+    '    "options": {\n' +
+    '      "A" : "IBAction resolves to void",\n' +
+    '      "B" : "IBAction is a type qualifier used by IB to enable connection user experience elements and app code.",\n' +
+    '      "C" : "IBAction is a macro defined to denote a method that can be referred to in Interface Builder.",\n' +
+    '      "D" : "None of the above"\n' +
+    '    },\n' +
+    '    "answer": "D"\n' +
+    '  },\n' +
+    '  {\n' +
+    '    "questionId": 15,\n' +
+    '    "question": "Which of the following statement is wrong?",\n' +
+    '    "options": {\n' +
+    '      "A" : "IBOutlet resolves to Id",\n' +
+    '      "B" : "IBOutlet is a type qualifier used by Interface Builder as a connection point for sending messages from app code to a user interface element.",\n' +
+    '      "C" : "IBOutlet are macro defined to denote a variable  that can be referred to in Interface Builder.",\n' +
+    '      "D" : "IBOutlet resolves to nothing"\n' +
+    '    },\n' +
+    '    "answer": "D"\n' +
+    '  },\n' +
+    '  {\n' +
+    '    "questionId": 16,\n' +
+    '    "question": "iOS remote push notification is introduced by apple in which version?",\n' +
+    '    "options": {\n' +
+    '      "A" : "iOS 3.0",\n' +
+    '      "B" : "iOS 6.0",\n' +
+    '      "C" : "iOS 2.0",\n' +
+    '      "D" : "iOS 4.0"\n' +
+    '    },\n' +
+    '    "answer": "A"\n' +
+    '  },\n' +
+    '  {\n' +
+    '    "questionId": 17,\n' +
+    '    "question": "Multitasking in iOS was introduced in which version?",\n' +
+    '    "options": {\n' +
+    '      "A" : "iOS 4.0",\n' +
+    '      "B" : "iOS 7.0",\n' +
+    '      "C" : "iOS 2.0",\n' +
+    '      "D" : "iOS 6.0"\n' +
+    '    },\n' +
+    '    "answer": "A"\n' +
+    '  },\n' +
+    '  {\n' +
+    '    "questionId": 18,\n' +
+    '    "question": "Mac OS stands for Macintosh Operating System.",\n' +
+    '    "options": {\n' +
+    '      "A" : "True",\n' +
+    '      "B" : "False"\n' +
+    '    },\n' +
+    '    "answer": "A"\n' +
+    '  },\n' +
+    '  {\n' +
+    '    "questionId": 19,\n' +
+    '    "question": "The programming language developed by Apple Inc. is ______.",\n' +
+    '    "options": {\n' +
+    '      "A" : "Android",\n' +
+    '      "B" : "Swift",\n' +
+    '      "C" : "Java",\n' +
+    '      "D" : "Python"\n' +
+    '    },\n' +
+    '    "answer": "B"\n' +
+    '  },\n' +
+    '  {\n' +
+    '    "questionId": 20,\n' +
+    '    "question": "Objective-C is the parent language of Swift.",\n' +
+    '    "options": {\n' +
+    '      "A" : "False",\n' +
+    '      "B" : "True"\n' +
+    '    },\n' +
+    '    "answer": "B"\n' +
+    '  }\n' +
+    ']\n';
